d7ed453ab84bedfb5718a053f569ba8e9cbf2a9e
Tom Dooner <tomdooner@gmail.com>
2022-02-02 20:10:30 -0800

    Refactor WordList specs

--- spec/word_list_spec.rb
+++ spec/word_list_spec.rb
@@ -1,18 +1,41 @@
 require 'spec_helper'
 
 RSpec.describe WordleInterviewQ::WordList do
-  describe '#matches_clue?' do
-    it 'returns the right answers' do
-      solver = described_class.new([])
-      expect(solver.send(:matches_clue?, 'PARTY', 'PXXXX', 'GNNNN')).to eq(true)
-      expect(solver.send(:matches_clue?, 'PARTY', 'PAAXX', 'GGNNN')).to eq(true)
-      expect(solver.send(:matches_clue?, 'PARTY', 'PXXXA', 'GNNNY')).to eq(true)
-      expect(solver.send(:matches_clue?, 'PARTY', 'PATXX', 'GGYNN')).to eq(true)
-      expect(solver.send(:matches_clue?, 'PARTY', 'XXYXX', 'NNYNN')).to eq(true)
-      expect(solver.send(:matches_clue?, 'HAREM', 'RAZER', 'YGNGN')).to eq(true)
-      expect(solver.send(:matches_clue?, 'CATTY', 'XTTXX', 'NYGNN')).to eq(true)
-      expect(solver.send(:matches_clue?, 'PARTY', 'PXXTX', 'GNNYN')).to eq(false)
-      expect(solver.send(:matches_clue?, 'PARTY', 'PXXXX', 'NNNNN')).to eq(false)
+  describe '#filter' do
+    let(:list) { ['PARTY'] }
+
+    subject { described_class.new(list) }
+
+    it 'does not remove for simple green clues' do
+      expect(subject.filter('PXXXX', 'GNNNN')).to include('PARTY')
+      expect(subject.filter('PAAXX', 'GGNNN')).to include('PARTY')
+    end
+
+    it 'does not remove for simple yellow clues' do
+      expect(subject.filter('PXXXA', 'GNNNY')).to include('PARTY')
+      expect(subject.filter('PATXX', 'GGYNN')).to include('PARTY')
+      expect(subject.filter('XXYXX', 'NNYNN')).to include('PARTY')
+    end
+
+    it 'removes the word for simple clues' do
+      expect(subject.filter('PXXTX', 'GNNYN').list).to be_empty
+      expect(subject.filter('PXXXX', 'NNNNN').list).to be_empty
+    end
+
+    context 'when the clue has repeated letters (Y/N)' do
+      let(:list) { ['HAREM'] }
+
+      it 'does not remove the word' do
+        expect(subject.filter('RAZER', 'YGNGN')).to include('HAREM')
+      end
+    end
+
+    context 'when the clue has repeated letters (G/Y)' do
+      let(:list) { ['CATTY'] }
+
+      it 'does not remove the word' do
+        expect(subject.filter('XTTXX', 'NYGNN')).to include('CATTY')
+      end
     end
   end
 end
