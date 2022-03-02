8ab22fbc14df4820c74db4ce97e8ba291aeaf8d8
Tom Dooner <tomdooner@gmail.com>
2022-03-01 17:28:29 -0800

    Unroll loops to improve performance
    I'm not exactly sure but by unrolling these loops the performance
    improves from
    
                          user     system      total        real
      #filter         2.793250   0.006403   2.799653 (  2.807531)
      Total allocations: 30446305 (30446/iter)
    
    To
    
                          user     system      total        real
      #filter         2.292048   0.006641   2.298689 (  2.308039)
      Total allocations: 28697332 (28697/iter)

--- lib/word_list.rb
+++ lib/word_list.rb
@@ -35,22 +35,33 @@ module WordleInterviewQ
     def matches_clue?(word, guess, clue)
       word_letters = word.chars
 
-      # Reject words missing 'G' letters
+      # Reject words with missing 'G' letters
+      return false if clue[0] == 'G' && word[0] != guess[0]
+      return false if clue[1] == 'G' && word[1] != guess[1]
+      return false if clue[2] == 'G' && word[2] != guess[2]
+      return false if clue[3] == 'G' && word[3] != guess[3]
+      return false if clue[4] == 'G' && word[4] != guess[4]
+
+      # Reject words with 'Y' letters that should be green
+      return false if clue[0] == 'Y' && word[0] == guess[0]
+      return false if clue[1] == 'Y' && word[1] == guess[1]
+      return false if clue[2] == 'Y' && word[2] == guess[2]
+      return false if clue[3] == 'Y' && word[3] == guess[3]
+      return false if clue[4] == 'Y' && word[4] == guess[4]
+
+      # Keep track of which letters were used in clues already, so we can use
+      # knowledge of the grey squares to eliminate words correctly even when
+      # the clue contains the same letter multiple times with different colors.
       5.times do |i|
-        next unless clue[i] == 'G'
-
-        return false unless word[i] == guess[i]
-        word_letters[i] = nil
-      end
-
-      # Reject words without remaining 'Y' letters or with 'Y' letters that
-      # would have been green.
-      5.times do |i|
-        next unless clue[i] == 'Y'
-
-        return false unless (clue_index = word_letters.index(guess[i])) && clue_index != i
-        return false if word[i] == guess[i]
-        word_letters[clue_index] = nil
+        case clue[i]
+        when 'G'
+          word_letters[i] = nil
+        when 'Y'
+          # Reject words without remaining 'Y' letters or with 'Y' letters that
+          # would have been green.
+          return false unless (clue_index = word_letters.index(guess[i])) && clue_index != i
+          word_letters[clue_index] = nil
+        end
       end
 
       # Reject words with 'N' letters
